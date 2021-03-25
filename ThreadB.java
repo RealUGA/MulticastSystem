987ac1ba49b3071205d8dbf2cc14eb10b43f09a4
AbheeRak <54927796+AbheeRak@users.noreply.github.com>
2021-03-24 20:13:39 -0400

    Updated Thread A And Thread B

--- ThreadB.java
+++ ThreadB.java
@@ -1,9 +1,42 @@
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class ThreadB {
+public class ThreadB implements Runnable {
+	private static int PORT ;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public ThreadB(int Port) throws IOException{
+		this.PORT = Port;
 	}
 
+	@Override
+	public void run() {
+		try {
+			ServerSocket serverSocket1 = new ServerSocket(PORT);
+			while (true) {
+				System.out.println("Server waiting for connection....");
+				Socket clientSocket1 = serverSocket1.accept();
+
+				BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket1.getInputStream()));
+				System.out.println("Server Connected");
+
+				FileWriter myWriter = new FileWriter("log.txt");
+//				System.out.println(in.readLine());
+				myWriter.write(in.readLine());
+
+				myWriter.close();
+				System.out.println("Message appended to the log.txt");
+
+
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+	}
 }
+
