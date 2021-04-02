2e34fb3a1474d86f469f471806b2d3e69440040b
AbheeRak <54927796+AbheeRak@users.noreply.github.com>
2021-04-02 19:05:06 -0400

    Some more updates on CW

--- CommandWorker.java
+++ CommandWorker.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*;
 
 
-public class CommandWorker implements Runnable{
+public class CommandWorker implements Runnable {
     private Socket client;
     private BufferedReader input;
     private PrintWriter outforb;
@@ -20,30 +20,32 @@ public class CommandWorker implements Runnable{
 
     }
 
-    public void register(String id,String IP, int port){
+    public void register(String id, String IP, int port) {
 
-        User member = new User(id,IP,port);
+        User member = new User(id, IP, port);
         Coordinator.userList.add(member);
         outfora.println("User Added");
 
     }
-    public void deregister(String id){
-        int length = Coordinator.userList.size();
-        for(int i = 0; i<=length; i++){
 
-            if(Coordinator.userList.get(i).getId().equals(id)){
+    public void deregister(String id) {
+        int length = Coordinator.userList.size();
+        for (int i = 0; i <= length; i++) {
+
+            if (Coordinator.userList.get(i).getId().equals(id)) {
                 Coordinator.userList.remove(i);
             }
         }
         outfora.println("User Removed");
 
     }
-    public void disconnect(String id){
+
+    public void disconnect(String id) {
         int length = Coordinator.userList.size();
 
-        for(int i = 0; i<=length; i++){
+        for (int i = 0; i <= length; i++) {
 
-            if(Coordinator.userList.get(i).getId().equals(id)){
+            if (Coordinator.userList.get(i).getId().equals(id)) {
                 Coordinator.userList.get(i).setStatus(false);
                 int last_message_id = Coordinator.mQueue.getLast().getId();
 
@@ -54,37 +56,36 @@ public class CommandWorker implements Runnable{
 
 
     }
-    public void reconnect(String ipofb, int portofb, String id) throws IOException{
+
+    public void reconnect(String ipofb, int portofb, String id) throws IOException {
 
         Socket threadb = new Socket(ipofb, portofb);
-        PrintWriter outforb = new PrintWriter(threadb.getOutputStream(),true);
+        PrintWriter outforb = new PrintWriter(threadb.getOutputStream(), true);
 
         int length = Coordinator.userList.size();
 
-        for(int i = 0; i<=length; i++){
+        for (int i = 0; i <= length; i++) {
 
-            if(Coordinator.userList.get(i).getId().equals(id)){
+            if (Coordinator.userList.get(i).getId().equals(id)) {
                 Coordinator.userList.get(i).setStatus(true);
                 Coordinator.userList.get(i).setPort(portofb);
                 int mqlength = Coordinator.mQueue.size();
                 boolean flag = false;
 
-                for(int j = 0; j<mqlength; j++){
-                    if(flag == true){
+                for (int j = 0; j < mqlength; j++) {
+                    if (flag == true) {
                         outforb.println(Coordinator.mQueue.get(j).getContents());
                     }
-                    if(Coordinator.mQueue.get(j).getMessageID == Coordinator.userList.get(i).getLastMessage()){
+                    if (Coordinator.mQueue.get(j).getMessageID == Coordinator.userList.get(i).getLastMessage()) {
                         flag = true;
                     }
-                    if(flag == false){
-                        for(int k = 0; k<mqlength; k++){
+                    if (flag == false) {
+                        for (int k = 0; k < mqlength; k++) {
                             outforb.println(Coordinator.mQueue.get(k).getContents());
 
                         }
                     }
                     threadb.close();
-
-
 
 
                 }
@@ -93,21 +94,22 @@ public class CommandWorker implements Runnable{
         }
 
     }
-    public void msend(String messagetobesent) throws IOException{
+
+    public void msend(String messagetobesent) throws IOException {
         int length = Coordinator.userList.size();
 
-        for(int i = 0; i<=length; i++){
+        for (int i = 0; i <= length; i++) {
 
-            if(Coordinator.userList.get(i).getStatus() == true){
+            if (Coordinator.userList.get(i).getStatus() == true) {
                 int port = Coordinator.userList.get(i).getPort();
                 String ip = Coordinator.userList.get(i).getIpAddress();
 
                 Socket threadb = new Socket(ip, port);
-                PrintWriter outforb = new PrintWriter(threadb.getOutputStream(),true);
+                PrintWriter outforb = new PrintWriter(threadb.getOutputStream(), true);
 
                 outforb.println(messagetobesent);
 
-                Message newmsg = new Message(Coordinator.messageNumber+1, messagetobesent);
+                Message newmsg = new Message(Coordinator.messageNumber + 1, messagetobesent);
                 Coordinator.messageNumber++;
                 Coordinator.mQueue.add(newmsg);
                 threadb.close();
@@ -119,32 +121,52 @@ public class CommandWorker implements Runnable{
 
     @Override
     public void run() {
+        try{
         input = new BufferedReader(new InputStreamReader(client.getInputStream()));// receives client input
-        outfora = new PrintWriter(client.getOutputStream(),true); // used to output to client
+        outfora = new PrintWriter(client.getOutputStream(), true); // used to output to client
 
-        ID = input[0];
-        OwnIP = input[1];
-        Command = input[2];
-        Addendum = input[3];
+        
+        int index = 0;
+        
+        fullCommand = input.readLine();
+        int length = fullCommand.length();
+        
+        index = fullCommand.indexOf(" ");
+        ID = fullCommand.substring(0,index);
 
-        if Command == "register"{
-            register();
+        fullCommand = fullCommand.substring(index + 1, length - 1);
+        OwnIP = fullCommand.substring(0,index);
+
+        fullCommand = fullCommand.substring(index + 1, length - 1);
+        Command = fullCommand.substring(0,index);
+
+        fullCommand = fullCommand.substring(index + 1, length - 1);
+        Addendum = fullCommand.substring(0,index);
+            
+
+        
+
+        if(Command.equals("register")) {
+            register(ID,OwnIP,Integer.parseInt(Addendum));
         }
-        if Command == "deregister"{
-            deregister();
+        if(Command.equals("deregister")) {
+            deregister(ID);
         }
-        if Command == "disconnect"{
-            disconnect();
+        if(Command.equals("disconnect")) {
+            disconnect(ID);
         }
-        if Command == "reconnect"{
-            reconnect();
+        if(Command.equals("reconnect")) {
+            reconnect(ID,OwnIP,Integer.parseInt(Addendum));
         }
-        if Command == "msend"{
-            msend();
+        if(Command.equals("msend")) {
+            msend(Addendum);
         }
 
 
+    }catch(IOException e){
 
-    }
+        }
+}
+
 }
 
